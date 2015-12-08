ae85fe8e29c4a4dc75ef28438539c4d48305385f
Sam Fuchs <samcfuchs@gmail.com>
2015-12-08 14:29:40 -0500

    Reverted back to joystick control

--- commands/drivewithjoysticks.py
+++ commands/drivewithjoysticks.py
@@ -10,10 +10,10 @@ class DriveWithJoysticks(Command):
         self.requires(self.robot.drive)
 
     def execute(self):
-        leftSpeed = self.robot.oi.controller.getRawAxis(1)
-        rightSpeed = self.robot.oi.controller.getRawAxis(5)
-        #leftSpeed = self.robot.oi.leftStick.getY()
-        #rightSpeed = self.robot.oi.rightStick.getY()
+        #leftSpeed = self.robot.oi.controller.getRawAxis(1)
+        #rightSpeed = self.robot.oi.controller.getRawAxis(5)
+        leftSpeed = self.robot.oi.leftStick.getY()
+        rightSpeed = self.robot.oi.rightStick.getY()
         self.robot.drive.setLeftSpeed(leftSpeed)
         self.robot.drive.setRightSpeed(rightSpeed)
 
